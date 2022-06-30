e4b97f9eae28ed2828fa1babc7bfe46eee8b2811
leegohi <10923930+leegohi@users.noreply.github.com>
2022-06-30 18:49:41 +0800

    ✨ add win copy

---
+++ utils/uwin.py
@@ -0,0 +1,15 @@
+from io import BytesIO
+import win32clipboard
+from PIL import Image
+
+def send_to_clipboard(clip_type, data:bytes):
+    win32clipboard.OpenClipboard()
+    win32clipboard.EmptyClipboard()
+    win32clipboard.SetClipboardData(clip_type, data)
+    win32clipboard.CloseClipboard()
+def win_copy_image(image:Image)->None:
+    output = BytesIO()
+    image.convert("RGB").save(output, "BMP")
+    data = output.getvalue()[14:]
+    output.close()
+    send_to_clipboard(win32clipboard.CF_DIB, data)
